1a90d0a304af8117dc42226b473133eeee5cca7d
Michael Dippery <michael@monkey-robot.com>
2022-12-28 19:08:49 -0800

    Move configuration to "tool.poetry_scm_version" section
    Custom attributes cannot be defined in the base "tool.poetry" section or
    Poetry will throw an error.

--- src/poetry_version_scm/plugin.py
+++ src/poetry_version_scm/plugin.py
@@ -1,3 +1,5 @@
+import typing as t
+
 from cleo.io.io import IO
 from poetry.plugins.plugin import Plugin
 from poetry.poetry import Poetry
@@ -7,7 +9,7 @@ from poetry_version_scm.scm.git import Git
 
 class VersionScmPlugin(Plugin):
     def activate(self, poetry: Poetry, io: IO) -> None:
-        if not poetry.pyproject.poetry_config.get("use_scm_version", False):
+        if not self.config(poetry, io).get("use_scm_version", False):
             return
 
         io.write_line(f"BEFORE: {poetry.package.version}")
@@ -16,3 +18,8 @@ class VersionScmPlugin(Plugin):
         version = desc.lstrip("v") if has_tag else "0.0.0"
         poetry.package.version = version
         io.write_line(f"AFTER: {poetry.package.version}")
+
+    def config(self, poetry: Poetry, io: IO) -> dict[str, t.Any]:
+        tool = poetry.pyproject.data["tool"]
+        config = tool["poetry_scm_version"]
+        return config
